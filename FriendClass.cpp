6e529b3d3b9b002a640f56dc20e29f0c893aaa58
mufid1012 <mufidghibran1012@gmail.com>
2024-05-07 15:36:29 +0700

    membuat class siswa private int id dan public void setId, displayAll

--- FriendClass.cpp
+++ FriendClass.cpp
@@ -11,3 +11,14 @@ public:
     void setNama(string pNama);
     friend class siswa;
 };
+
+class siswa 
+{
+private:
+    int id;
+
+public:
+    void setId(int pId);
+    void displayAll(orang &a);
+
+};
